63ded3924d851f2e0d38f9f2580edca7c3bbf2d5
Mansi Agrawal <agrawal.ma@husky.neu.edu>
2021-01-27 16:53:36 -0500

    updating about us

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,17 @@ const About = () => {
     <section className='section about-section'>
       <h1 className='section-title'>about us</h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
-        distinctio numquam rerum laboriosam in debitis, consequuntur natus
-        dignissimos explicabo illo id laudantium? Maiores temporibus dolorem,
-        unde iure numquam quas quo aspernatur deserunt minima optio voluptates
-        tempora nisi reprehenderit odit culpa!
+        CocktailDB is a cocktail search and recipe resource, with over 500
+        amazing recipes. We are adding more ingredient everyday and we plan to
+        make it one of the biggest community of Cocktail enthusiasts. The Goal
+        of CocktailDB is to allow Cocktail enthusiasts to explore and try out
+        new drinks and have a good time. CocktailDB is founded by cocktail and
+        wine enthusiast and software developer Mansi Agrawal.
+        <br></br>
+        <br></br>Mansi is a Northeastern Graduate student, who enjoys writing
+        code and building new things while trying out amazing recipes for
+        cocktails. Check her LinkedIn profile and shoot an email if you would
+        like to get in touch.
       </p>
     </section>
   )
